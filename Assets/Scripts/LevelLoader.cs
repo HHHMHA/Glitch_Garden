0209092aeaf6e932eaa4d1907ebaa7374f833abc
J2mF <mp4-12cs5@hotmail.com>
2021-05-12 19:50:19 +0300

    Initial Commit
    Add Splash Screen
    Add Start Game Screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private const int StartGameSceneIndex = 1;
+    [SerializeField] private float loadDelay = 1f;
+        
+    public void LoadStartGameScreenWithDelay() {
+        StartCoroutine( LoadSceneWithDelay( StartGameSceneIndex ) );
+    }
+
+    private IEnumerator LoadSceneWithDelay( int sceneIndex ) {
+        yield return new WaitForSeconds( loadDelay );
+        SceneManager.LoadScene( sceneIndex );
+    }
+}
---
+++ Assets/Scripts/StartScreenLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenLoader : MonoBehaviour {
+    [SerializeField] private LevelLoader levelLoader;
+    void Start()
+    {
+        levelLoader.LoadStartGameScreenWithDelay();
+    }
+}
