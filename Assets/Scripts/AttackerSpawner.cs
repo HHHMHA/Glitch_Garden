e1fddd085375ee72bdc98c6d88961ec2eb51d422
J2mF <mp4-12cs5@hotmail.com>
2021-07-21 23:26:13 +0300

    Add AttackerSpawner
    Add Prefabs

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int minSpawnDelay = 1;
+    [SerializeField] private int maxSpawnDelay = 5;
+
+    private bool spawn = true;
+
+
+    private void Start() {
+        StartCoroutine( Spawner() );
+    }
+
+    private IEnumerator Spawner() {
+        while ( spawn ) {
+            Instantiate( prefab, transform.position, Quaternion.identity );
+            minSpawnDelay = 1;
+            yield return new WaitForSeconds( Random.Range( minSpawnDelay, maxSpawnDelay ) );
+        }
+    }
+}
