d2b88a39925074fd11fe3975781074877a3b4aa0
J2mF <mp4-12cs5@hotmail.com>
2021-07-21 17:51:20 +0300

    Implement Enemy Movement in Attacker.cs

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+    [SerializeField]
+    [Range( 0f, 5f )] 
+    private float speed = .5f;
+
+    private void Update() {
+        transform.Translate( Vector2.left * (speed * Time.deltaTime) );
+    }
+}
