684aa40fab59e7a7ccc0b027cd66ef70be9117e6
J2mF <mp4-12cs5@hotmail.com>
2021-09-01 21:07:51 +0300

    Add Cactus Projectile

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    [SerializeField] private GameObject projectile;
+    
+    public void Fire() {
+        Instantiate( projectile, transform.GetChild( 0 ).position, Quaternion.identity );
+    }
+}
