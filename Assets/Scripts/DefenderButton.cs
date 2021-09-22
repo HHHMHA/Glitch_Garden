1ab8845c174cae7ce5bd7d7486c4eb2ac208f15a
J2mF <mp4-12cs5@hotmail.com>
2021-09-22 15:06:13 +0300

    Add Defender Buttons UI

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour {
+    private static readonly Color32 selectedColor = Color.white;
+    private static readonly Color32 inactiveColor = new Color32( 130, 130, 130, 255 );
+    
+    private void OnMouseDown() {
+        ToggleSelected();
+    }
+    
+    public void ToggleSelected() {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (var button in buttons) {
+            button.SetInactive();
+        }
+
+        SetSelected();
+    }
+
+    public void SetSelected() {
+        GetComponent<SpriteRenderer>().color = selectedColor;
+    }
+    
+    public void SetInactive() {
+        GetComponent<SpriteRenderer>().color = inactiveColor;
+    }
+}
