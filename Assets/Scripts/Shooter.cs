c02f94b75630fadd8ebc333a190c67942ee436a4
J2mF <mp4-12cs5@hotmail.com>
2021-10-13 21:39:49 +0300

    Change Prefab Structures
    Change Log:
    - Make Prefabs More Flexible For Animations

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,6 +4,6 @@ public class Shooter : MonoBehaviour {
     [SerializeField] private GameObject projectile;
     
     public void Fire() {
-        Instantiate( projectile, transform.GetChild( 0 ).position, Quaternion.identity );
+        Instantiate( projectile, transform.GetChild( 0 ).GetChild( 0 ).position, Quaternion.identity );
     }
 }
