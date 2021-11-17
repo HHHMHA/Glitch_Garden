4cae3d00384086875548d9a7159557b8dcf5dc32
J2mF <mp4-12cs5@hotmail.com>
2021-11-17 18:33:06 +0200

    Add Lizard Attacking Animation

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,7 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Attacker : MonoBehaviour {
     private float currentSpeed = 0f;
+    private GameObject currentTarget;
+    private Animator animator;
+    private static readonly int IsAttacking = Animator.StringToHash( "isAttacking" );
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update() {
         transform.Translate( Vector2.left * ( currentSpeed * Time.deltaTime ) );
@@ -9,5 +17,10 @@ public class Attacker : MonoBehaviour {
 
     public void SetMovementSpeed( float speed ) {
         currentSpeed = speed;
+    }
+
+    public void Attack( GameObject target ) {
+        animator.SetBool( IsAttacking, true );
+        currentTarget = target;
     }
 }
---
+++ Assets/Scripts/Lizard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class Lizard : MonoBehaviour {
+        private void OnTriggerEnter2D( Collider2D other ) {
+                var target = other.gameObject;
+                if ( target.GetComponent<Defender>() == null ) return;
+                var attacker = GetComponent<Attacker>();
+                attacker.Attack( target );
+        }
+}
